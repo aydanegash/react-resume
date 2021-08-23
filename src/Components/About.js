95bcd46c5c45bfbff99768308e883b648278f413
aydanegash <73369360+aydanegash@users.noreply.github.com>
2021-08-23 16:26:36 -0500

    updated resume and contact page and profile pic

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,10 +8,6 @@ class About extends Component {
     const name = this.props.data.name;
     const profilepic = "images/" + this.props.data.image;
     const bio = this.props.data.bio;
-    const street = this.props.data.address.street;
-    const city = this.props.data.address.city;
-    const state = this.props.data.address.state;
-    const zip = this.props.data.address.zip;
     const phone = this.props.data.phone;
     const email = this.props.data.email;
     const resumeDownload = this.props.data.resumedownload;
@@ -24,7 +20,7 @@ class About extends Component {
               <img
                 className="profile-pic"
                 src={profilepic}
-                alt="Nordic Giant Profile Pic"
+                alt="Ayda Negash Profile pic"
               />
             </div>
             <div className="nine columns main-col">
@@ -32,22 +28,16 @@ class About extends Component {
 
               <p>{bio}</p>
               <div className="row">
-                <div className="columns contact-details">
+                {/* <div className="columns contact-details">
                   <h2>Contact Details</h2>
                   <p className="address">
                     <span>{name}</span>
-                    <br />
-                    <span>
-                      {street}
-                      <br />
-                      {city} {state}, {zip}
-                    </span>
-                    <br />
+                    <br/>
                     <span>{phone}</span>
-                    <br />
+                    <br/>
                     <span>{email}</span>
                   </p>
-                </div>
+                </div> */}
                 <div className="columns download">
                   <p>
                     <a href={resumeDownload} className="button">
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,20 +8,33 @@ class Contact extends Component {
     const name = this.props.data.name;
     const phone = this.props.data.phone;
     const email = this.props.data.email;
+    const github = this.props.data.github;
+    const resumeDownload = this.props.data.resume;
 
      return (
 
       <section id="contact">
           <Fade bottom duration={1000}>
             <Slide right duration={1000}>
-              <aside className="four columns footer-widgets">
-                <div className="widget widget_contact">
-                  <h4>Contact Info</h4>
-                  <p className="address">
-                    {name} <br/>
-                    {email} <br/>
-                    <span>{phone}</span>
+              <aside>
+                <div className="row">
+                  <div className="columns contact-details">
+                      <h2>Contact Details</h2>
+                        <p className="address">
+                          <span>{name}</span>
+                          <br/>
+                          <span>{phone}</span>
+                          <br/>
+                          <span>{email}</span>
+                        </p>
+                  </div>
+                  <div className="columns download">
+                  <p>
+                    <a href={resumeDownload} className="button">
+                      <i className="fa fa-download"></i>Download Resume
+                    </a>
                   </p>
+                </div>
                 </div>
               </aside>
             </Slide>
